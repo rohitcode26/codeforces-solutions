bc16a20ebe6f8370a50bf9b7dc804e1dc62dae18
rohitcode26 <47224960+rohitcode26@users.noreply.github.com>
2020-05-25 01:19:00 +0530

    Add files via upload

---
+++ Board Moves.cpp
@@ -0,0 +1,126 @@
+#include<bits/stdc++.h>
+ 
+using namespace std ;
+ 
+typedef double D ;
+typedef long long ll ;
+typedef long double ld ;
+typedef unsigned int ui ;
+typedef unsigned long long ull ;
+ 
+# define F first
+# define S second
+# define R return
+# define C continue 
+# define pb push_back 
+# define pf push_front
+# define mp make_pair
+ 
+# define vi vector <int>
+# define vb vector <bool>
+# define vc vector <char>
+# define vll vector <ll>
+# define vs vector <string>
+ 
+# define vvi vector < vector < int > >
+# define vvb vector < vector < bool > >
+# define vvc vector < vector < char > >
+# define vvll vector < vector < ll > >
+# define vvd vector < vector < D > >
+# define vvld vector < vector < ld > >
+ 
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+# define pld pair < ld , ld >
+# define pDD pair < D , D >
+ 
+# define vpld vector < pld >
+# define vpii vector < pii >
+# define vpll vector < pll >
+# define vpDD vector < pDD >
+# define vvpii vector < vector < pii > >
+ 
+# define all(v) (v).begin() , (v).end()
+# define allrev(v) (v).rbegin() , (v).rend()
+# define allcomp(v) v.begin() , v.end() , comp
+# define allrevcomp(v) v.rbegin() , v.rend() , comp
+
+# define dist(a,b,p,q) sqrt((p-a)*(p-a) + (q-b)*(q-b))
+
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+ 
+const ll MOD = 1e9 + 7 ;
+const int infi =  INT_MAX ;
+const ll infll =  LLONG_MAX ;
+const ld PI = 3.1415926535897932384626 ;
+
+const int dx[] = {-1 , 0 , 1 , 0} ;
+const int dy[] = {0 , 1 , 0 , -1} ;
+ 
+///////////////////////////////////////////////////////////////////////////////////////
+
+ll pow(ll a , ll b , ll m) {
+	ll ans = 1 ;
+	while(b)
+	{
+		if (b&1) {
+			ans = (ans*a) % m ;
+		}
+		b /= 2 ;
+		a = (a*a) % m ;
+	}
+	return ans ;
+}
+
+bool isprime(ll n) {
+    for (ll i = 2 ; i * i <= n ; ++i)
+        if (n % i == 0) {
+        	return false ;
+		}
+    return true;
+}
+
+ll gcd(ll a, ll b) {
+    if (b == 0) {
+    	return a ;
+	}
+    else {
+    	return gcd (b , a % b) ;
+	}
+}
+
+ll lcm(ll a, ll b) {
+    return ((a / gcd(a , b)) * b) ;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+void solve (int test_case) {
+	int n ;
+	cin >> n ;
+	if (n == 1) {
+		cout << 0 << '\n' ;
+		return ;
+	}
+	ll moves = 0 ;
+	ll layer = 8 ;
+	for (int i = 1 ; i <= n/2 ; ++i) {
+		moves += layer*i ;
+		layer += 8 ;
+	}
+	cout << moves << '\n' ;
+}
+
+int main() {
+	FAST ;
+	//#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//#endif
+	int tc = 1 ;
+	cin >> tc ;
+	while (tc--) {
+		solve (tc) ;
+	}
+	return 0 ;
+}
---
+++ Orac and LCM.cpp
@@ -0,0 +1,133 @@
+#include<bits/stdc++.h>
+ 
+using namespace std ;
+ 
+typedef double D ;
+typedef long long ll ;
+typedef long double ld ;
+typedef unsigned int ui ;
+typedef unsigned long long ull ;
+ 
+# define F first
+# define S second
+# define R return
+# define C continue 
+# define pb push_back 
+# define pf push_front
+# define mp make_pair
+ 
+# define vi vector <int>
+# define vb vector <bool>
+# define vc vector <char>
+# define vll vector <ll>
+# define vs vector <string>
+ 
+# define vvi vector < vector < int > >
+# define vvb vector < vector < bool > >
+# define vvc vector < vector < char > >
+# define vvll vector < vector < ll > >
+# define vvd vector < vector < D > >
+# define vvld vector < vector < ld > >
+ 
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+# define pld pair < ld , ld >
+# define pDD pair < D , D >
+ 
+# define vpld vector < pld >
+# define vpii vector < pii >
+# define vpll vector < pll >
+# define vpDD vector < pDD >
+# define vvpii vector < vector < pii > >
+ 
+# define all(v) (v).begin() , (v).end()
+# define allrev(v) (v).rbegin() , (v).rend()
+# define allcomp(v) v.begin() , v.end() , comp
+# define allrevcomp(v) v.rbegin() , v.rend() , comp
+
+# define dist(a,b,p,q) sqrt((p-a)*(p-a) + (q-b)*(q-b))
+
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+ 
+const ll MOD = 1e9 + 7 ;
+const int infi =  INT_MAX ;
+const ll infll =  LLONG_MAX ;
+const ld PI = 3.1415926535897932384626 ;
+
+const int dx[] = {-1 , 0 , 1 , 0} ;
+const int dy[] = {0 , 1 , 0 , -1} ;
+ 
+///////////////////////////////////////////////////////////////////////////////////////
+
+ll pow(ll a , ll b , ll m) {
+	ll ans = 1 ;
+	while(b)
+	{
+		if (b&1) {
+			ans = (ans*a) % m ;
+		}
+		b /= 2 ;
+		a = (a*a) % m ;
+	}
+	return ans ;
+}
+
+bool isprime(ll n) {
+    for (ll i = 2 ; i * i <= n ; ++i)
+        if (n % i == 0) {
+        	return false ;
+		}
+    return true;
+}
+
+ll gcd(ll a, ll b) {
+    if (b == 0) {
+    	return a ;
+	}
+    else {
+    	return gcd (b , a % b) ;
+	}
+}
+
+ll lcm(ll a, ll b) {
+    return ((a / gcd(a , b)) * b) ;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+void solve (int test_case) {
+	int n ;
+	cin >> n ;
+	vll v (n) ;
+	for (int i = 0 ; i < n ; ++i) {
+		cin >> v[i] ;
+	}
+	vll suff_gcd (n) ;
+	suff_gcd[n-1] = v[n-1] ;
+	for (int i = n-2 ; i >= 0 ; --i) {
+		suff_gcd[i] = __gcd (v[i] , suff_gcd[i+1]) ;
+	}
+	vll lcm_arr (n) ;
+	for (int i = 0 ; i < n-1 ; ++i) {
+		lcm_arr[i] = (v[i] * suff_gcd[i+1]) / __gcd (v[i] , suff_gcd[i+1]) ;
+	}
+	ll ans = lcm_arr[0] ;
+	for (int i = 1 ; i < n-1 ; ++i) {
+		ans = __gcd (ans , lcm_arr[i]) ;
+	}
+	cout << ans << '\n' ;
+}
+
+int main() {
+	FAST ;
+	//#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//#endif
+	int tc = 1 ;
+//	cin >> tc ;
+	while (tc--) {
+		solve (tc) ;
+	}
+	return 0 ;
+}
